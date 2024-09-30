019c742677e8f5ab4a6462642c737e18dd9c6d71
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-09-30 09:24:55 +0700

    backend re-deploy

--- backend/src/main/java/com/example/config/WebConfig.java
+++ backend/src/main/java/com/example/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://bt2701.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
